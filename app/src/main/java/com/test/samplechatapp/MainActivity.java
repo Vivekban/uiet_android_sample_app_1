35ca6425d0c6d1bbb8be38500b8494646b3466b1
msaif17 <saifansari0517@gmail.com>
2016-05-03 12:31:23 +0530

    Authentication using phone number

--- app/src/main/java/com/test/samplechatapp/MainActivity.java
+++ app/src/main/java/com/test/samplechatapp/MainActivity.java
@@ -1,19 +1,30 @@
 package com.test.samplechatapp;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements View.OnClickListener{
 
- 
+
+    Button b1;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
         //hello kain
         //hello saif
+        b1=(Button)findViewById(R.id.button);
+        b1.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        Intent i=new Intent(getApplicationContext(),PhoneAuthentication.class);
+        startActivity(i);
     }
 }
